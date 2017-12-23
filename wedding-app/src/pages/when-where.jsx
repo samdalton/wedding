faaff37a8dc1b31750c394374220fc1a3fc72a13
Sam Dalton <sam@aboutlife.com>
2017-12-23 09:22:54 -0800

    better mobile styling

--- wedding-app/src/pages/when-where.jsx
+++ wedding-app/src/pages/when-where.jsx
@@ -16,6 +16,14 @@ export const WhenWhere = () => (
     <p>A shuttle will pick up guests at <Link href="https://www.google.com/maps/place/Turtle+Bay+Resort/@21.7013641,-157.9891112,15z/data=!4m12!1m6!3m5!1s0x7c00504fe1e96419:0x1ee8f68b7f5c0bec!2sTurtle+Bay+Resort!8m2!3d21.704397!4d-157.9979769!3m4!1s0x7c00504fe1e96419:0x1ee8f68b7f5c0bec!8m2!3d21.704397!4d-157.9979769?hl=en">Turtle Bay Resort</Link> and return after the event. There is <em>no</em> parking at the venue.</p>
 
     <h2>Map of Venue</h2>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3708.5579637427654!2d-158.02792499999998!3d21.642142000000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c005a3eeeb9a727%3A0x4587a6ae83cc7273!2sSunset+Ranch+Hawaii!5e0!3m2!1sen!2sus!4v1511552101374" width="600" height="450" frameborder="0" title="map" style={{border: 0}} allowfullscreen></iframe>
+    <iframe
+      className="map"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3708.5579637427654!2d-158.02792499999998!3d21.642142000000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c005a3eeeb9a727%3A0x4587a6ae83cc7273!2sSunset+Ranch+Hawaii!5e0!3m2!1sen!2sus!4v1511552101374"
+      width="600"
+      height="450"
+      frameborder="0"
+      title="map"
+      style={{border: 0}}
+    ></iframe>
   </Wrapper>
 );
