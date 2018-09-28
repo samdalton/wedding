fcc9011e77005938619c66ab1ca1c9a0e6599a7b
Sam Dalton <sam@aboutlife.com>
2018-09-28 08:50:43 -0700

    add more details

--- wedding-app/src/pages/when-where.jsx
+++ wedding-app/src/pages/when-where.jsx
@@ -10,7 +10,9 @@ export const WhenWhere = () => (
     <h2>The Day Before - Thursday the 4th of October 2018</h2>
     <p>We will be having a casual afternoon get-together the Thursday before the wedding so we can unwind and relax before the big day ahead.</p>
     <p>Join us for snacks and refreshments any time between 2pm and 5pm at our Turtle Bay Condo:</p>
-    <p style={{ paddingLeft: 20 }}>57-77 Lalo Kuilima Pl
+    <p style={{ paddingLeft: 20 }}>
+      Kuilima Estates East #42
+      57-77 Lalo Kuilima Pl
       Kahuku , HI 96731</p>
     <p>We won't have space for everyone at once, so feel free to come and go as you please throughout the afternoon. We will likely grab dinner in the resort later on which we'll largely be playing by ear - this is Hawaii after all!</p>
     <p>If you have trouble finding our place, give Sam or Angela a call at +! 415 483 5962 and +1 925 899 8340 respectively.</p>
@@ -21,7 +23,7 @@ export const WhenWhere = () => (
       The date is <time>Friday the 5th of October, 2018</time>.{' '}
       <a className="subtext" href="/static/invite.ics">(Download calendar event)</a>
     </p>
-    <p>Be at Turtle Bay Resort at <time>3pm</time> for shuttles to the venue. The ceremony will start at <time>4pm</time>. The event will end at <time>9pm</time> with shuttles to return you back to Turtle Bay for further salubriations at the hotel bar.</p>
+    <p>Be at the lobby of Turtle Bay Resort at <time>3pm</time> for shuttles to the venue. The ceremony will start at <time>4pm</time>. The event will end at <time>9pm</time> with shuttles to return you back to Turtle Bay for further salubriations at the hotel bar.</p>
     <p>The shuttle is the only option to getting to the venue due to narrow roads and limited parking. You can find it on the map at <Link href="https://www.google.com/maps/place/Turtle+Bay+Resort/@21.7013641,-157.9891112,15z/data=!4m12!1m6!3m5!1s0x7c00504fe1e96419:0x1ee8f68b7f5c0bec!2sTurtle+Bay+Resort!8m2!3d21.704397!4d-157.9979769!3m4!1s0x7c00504fe1e96419:0x1ee8f68b7f5c0bec!8m2!3d21.704397!4d-157.9979769?hl=en">Turtle Bay Resort</Link>.</p>
     <p>Our wedding is children friendly and they'll have plenty of space to run around and entertain themselves.</p>
 
