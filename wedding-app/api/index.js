221879cc7990914bb531f28f995571687b207177
Sam Dalton <sam@aboutlife.com>
2018-11-27 19:37:38 -0800

    Fix api

--- wedding-app/api/index.js
+++ wedding-app/api/index.js
@@ -4,22 +4,16 @@ import path from 'path';
 import { readFile } from 'fs';
 
 const app = express();
-app.use(bodyParser.json());
 
 let indexHtml;
 readFile(path.join(__dirname, '..', 'build', 'index.html'), (err, data) => {
   indexHtml = data.toString();
 });
 
-initDB(app, (connection) => {
-  app.get('/*', (req, res, next) => {
-      return res.send(indexHtml);
-    }
-
-    return res.redirect('/fail');
-  });
-
-  app.use(compression());
-  app.use(express.static(path.join(__dirname, '..', 'build')));
-  app.listen(3001);
+app.get('/*', (req, res, next) => {
+    return res.send(indexHtml);
 });
+
+app.use(compression());
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.listen(3001);
